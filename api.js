714733fc025041b6c70f0f351ea874b9db2def48
unknown <shahariar.emn990@gmail.com>
2023-08-30 16:49:20 +0600

    m

--- api.js
+++ api.js
@@ -1,12 +1,47 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     console.log(phones);
-    const res1 = await fetch('https://openapi.programming-hero.com/api/phones?search=samsung');
-    const data1 = await res1.json();
-    const phones1 = data1.data;
-    console.log(phones1);
+    displayPhones(phones)
+}
+const searchButtonClicked = () => {
+    loader(true);
+    const searchText = document.getElementById('search-field').value;
+    loadPhone(searchText);
 }
 
+const displayPhones = (phones) => {
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    phones.forEach(phone => {
+        // console.log(phone);
+        // 2 create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
+        // 3: set inner html
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-center">
+                <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
+            </div>
+        </div>
+        `;
+        // 4 append child
+        phoneContainer.appendChild(phoneCard);
+    });
+    loader(false);
+}
+const loader = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner')
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+    }
+    else {
+        loadingSpinner.classList.add('hidden');
+    }
+}
 loadPhone()
